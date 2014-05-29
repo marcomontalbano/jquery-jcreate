dbfe8213430819c3e6c471a8fd82770bc4187a21
Marco Montalbano <marcomontalbano.work@gmail.com>
2014-05-29 16:44:26 +0200

    Refactored jCreate without test :(

--- src/jquery.jCreate.js
+++ src/jquery.jCreate.js
@@ -1,102 +1,198 @@
-// written by marco montalbano (marcomontalbano.it)
-// http://www.marcomontalbano.com/
-// version 1.0 - 18/11/2011
+/**
+ * jCreate | jQuery Special Event
+ *
+ * author: Marco Montalbano (www.marcomontalbano.com)
+ * first release: version 1.0 - 18/11/2011
+ *
+ * useful links
+ * ------------
+ * http://learn.jquery.com/events/event-extensions/
+ * http://benalman.com/news/2010/03/jquery-special-events/
+ *
+ */
 
-(function($, _domManip, _html){
-	var selectors = [], handler = [];
+(function($, domManip, html)
+{
+    var
+        container = [],
+        random    = Math.random()
+    ;
 
-	$.event.special.create = {
-		setup: function( data, namespaces, eventHandle ) {
-		},
-		teardown: function( namespaces ) {
-		},
-		add: function( handleObj ){
-			selectors.push( handleObj.selector );
-			handler.push( handleObj.handler );
-			$(function() {
-				if ($.inArray(handleObj.selector, selectors) >= 0)
-					$(handleObj.selector).each(handleObj.handler);
-			});
-		},
-		// won't fire in 1.4.2 http://dev.jquery.com/ticket/6202
-		remove: function( handleObj ){
-			var len = selectors.length;
-			while( len-- ){
-				if( selectors[len] === handleObj.selector ){
-					selectors.splice(len, 1);
-					handler.splice(len, 1);
-					break;
-				}
-			}
-		},
-		_default: function( event ){
-		}
-	};
-	
-	// DOM manipulation methods
-	$.fn.domManip = function( args, table, callback ){
-		var __m_domManip = _domManip.apply( this, arguments );
-		__m_domManip = check(args[0], __m_domManip);
-		return __m_domManip;
-	};
-	
-	// HTML method
-	$.fn.html = function( value ){
-		var __m_html = _html.apply( this, arguments );
-		__m_html = check(value, __m_html);
-		return __m_html;
-	};
-	
-	function check(value, __m)
-	{
-		if ( selectors.length > 0 )
-		{
-			if (typeof value == "string")
-			{
-				value = $(value);
-				if ( value.length > 0 )
-				{
-					var selectorFound = [], current = -1, obj = {};
-					
-					for (key in selectors)
-					{
-						current = selectors[key];
-						if ((value.is(current)) || (value.find(current).length > 0))
-							selectorFound.push(key);
-					}
-					
-					for (key in selectorFound)
-					{
-						current = selectorFound[key];
-						obj = $('body').find(selectors[current]);
-						if ( obj.length > 0 )
-							obj.each(handler[current]);
-					}
-					//alert('aa');
-					return $('body').find(__m);
-					
-					/*
-					var selClasses = value.attr('class').split(' ');
-					var index = 0, selClass = '', position = -1;
-					for (key in selClasses)
-					{
-						selClass = selClasses[key];
-						index = $.inArray('.' + selClass, selectors);
-						if ( index >= 0 )
-							continue;
-					}
-					
-					if ( index >= 0 ) {
-						var obj = __m.find(selectors[index]);
-						if ( obj.length > 0 )
-							obj.removeClass(selectors[index].replace('.','')).each(handler[index]);
-					}
-					*/
-				}
-			}
-		}
-		
-		return __m;
-	}
-	
+    $.event.special.create =
+    {
+        /**
+         * setup: function( data: Object, namespaces, eventHandle: function )
+         *  The setup hook is called the first time an event of a particular type is attached to an element;
+         *  this provides the hook an opportunity to do processing that will apply to all events of this type on this element.
+         *  The this keyword will be a reference to the element where the event is being attached and eventHandle is jQuery's event handler function.
+         *  In most cases the namespaces argument should not be used, since it only represents the namespaces of the first event being attached;
+         *  subsequent events may not have this same namespaces.
+         *  
+         *  This hook can perform whatever processing it desires,
+         *  including attaching its own event handlers to the element or to other elements and recording setup information on the element using the jQuery.data() method.
+         *  If the setup hook wants jQuery to add a browser event (via addEventListener or attachEvent, depending on browser) it should return false. In all other cases,
+         *  jQuery will not add the browser event, but will continue all its other bookkeeping for the event.
+         *  This would be appropriate, for example, if the event was never fired by the browser but invoked by .trigger().
+         *  To attach the jQuery event handler in the setup hook, use the eventHandle argument.
+         */
+        setup: function( data, namespaces, eventHandle )
+        {
+
+        },
+
+        /**
+         * teardown: function()
+         *  The teardown hook is called when the final event of a particular type is removed from an element.
+         *  The this keyword will be a reference to the element where the event is being cleaned up.
+         *  This hook should return false if it wants jQuery to remove the event from the browser's event system (via removeEventListener or detachEvent).
+         *  In most cases, the setup and teardown hooks should return the same value.
+         *  
+         *  If the setup hook attached event handlers or added data to an element through a mechanism such as jQuery.data(), 
+         *  the teardown hook should reverse the process and remove them.
+         *  jQuery will generally remove the data and events when an element is totally removed from the document,
+         *  but failing to remove data or events on teardown will cause a memory leak if the element stays in the document.
+         */
+        teardown: function()
+        {
+
+        },
+
+        /**
+         * add: function( handleObj )
+         *  Each time an event handler is added to an element through an API such as .on(), jQuery calls this hook.
+         *  The this keyword will be the element to which the event handler is being added,
+         *  and the handleObj argument is as described in the section above.
+         *  The return value of this hook is ignored.
+         */
+        add: function( handleObj )
+        {
+            container.push({
+                $element  : $(this),
+                handleObj : handleObj
+            });
+        },
+
+        /**
+         * move: function( handleObj )
+         *  When an event handler is removed from an element using an API such as .off(), this hook is called.
+         *  The this keyword will be the element where the handler is being removed,
+         *  and the handleObj argument is as described in the section above.
+         *  The return value of this hook is ignored.
+         */
+        remove: function( handleObj )
+        {
+            var len = container.length;
+            while( len-- )
+            {
+                if( $(this).is(container[len].$element) && container[len].handleObj.selector === handleObj.selector )
+                {
+                    container.splice(len, 1);
+                    break;
+                }
+            }
+        },
+
+        /**
+         * trigger: function( event: jQuery.Event, data: Object )
+         *  Called when the .trigger() or .triggerHandler() methods are used to trigger an event for the special type from code,
+         *  as opposed to events that originate from within the browser.
+         *  The this keyword will be the element being triggered, and the event argument will be a jQuery.Event object constructed from the caller's input.
+         *  At minimum, the event type, data, namespace, and target properties are set on the event.
+         *  The data argument represents additional data passed by .trigger() if present.
+         *
+         *  The trigger hook is called early in the process of triggering an event,
+         *  just after the jQuery.Event object is constructed and before any handlers have been called.
+         *  It can process the triggered event in any way, for example by calling event.stopPropagation() or event.preventDefault() before returning.
+         *  If the hook returns false, jQuery does not perform any further event triggering actions and returns immediately.
+         *  Otherwise, it performs the normal trigger processing, calling any event handlers for the element and bubbling
+         *  the event (unless propagation is stopped in advance or noBubble was specified for the special event) to call event handlers attached to parent elements.
+         */
+        trigger: function( event, data )
+        {
+
+        },
+
+        /**
+         * _default: function( event: jQuery.Event, data: Object )
+         *  When the .trigger() method finishes running all the event handlers for an event,
+         *  it also looks for and runs any method on the target object by the same name unless of the handlers called event.preventDefault().
+         *  So, .trigger( "submit" ) will execute the submit() method on the element if one exists.
+         *  When a _default hook is specified, the hook is called just prior to checking for and executing the element's default method.
+         *  If this hook returns the value false the element's default method will be called; otherwise it is not.
+         */
+        _default: function( event, data )
+        {
+
+        },
+
+        /**
+         * handle: function( event: jQuery.Event, data: Object )
+         *  jQuery calls a handle hook when the event has occurred and jQuery would normally call the user's event handler
+         *  specified by .on() or another event binding method. If the hook exists, jQuery calls it instead of that event handler,
+         *  passing it the event and any data passed from .trigger() if it was not a native event.
+         *  The this keyword is the DOM element being handled, and event.handleObj property has the detailed event information.
+         *
+         *  Based in the information it has, the handle hook should decide whether to call the original handler function which is in event.handleObj.handler.
+         *  It can modify information in the event object before calling the original handler,
+         *  but must restore that data before returning or subsequent unrelated event handlers may act unpredictably.
+         *  In most cases, the handle hook should return the result of the original handler, but that is at the discretion of the hook.
+         *  The handle hook is unique in that it is the only special event function hook that is called under its original special event name
+         *  when the type is mapped using bindType and delegateType. For that reason, it is almost always an error to have anything other
+         *  than a handle hook present if the special event defines a bindType and delegateType, since those other hooks will never be called.
+         */
+        handle: function( event, data )
+        {
+
+        }
+    };
+    
+    
+    // DOM manipulation methods
+    $.fn.domManip = function( args, table, callback )
+    {
+        var _domManip = domManip.apply( this, arguments );
+        return create(args[0], _domManip);
+    };
+    
+    // HTML method
+    $.fn.html = function( value )
+    {
+        var _html = html.apply( this, arguments );
+        return create(value, _html);
+    };
+
+    var create = function(value, _)
+    {
+        if (container.length >= 1)
+        {
+            var
+                $value   = $(value),
+                current  = null
+            ;
+            
+            for (var key in container)
+            {
+                current = container[key];
+                current.$element.find(current.handleObj.selector).each(function()
+                {
+                    var
+                        $this = $(this),
+                        data_key = '$.event.special.create',
+                        data_sep = ',',
+                        data  = $this.data(data_key) ? $this.data(data_key).split(data_sep) : []
+                    ;
+
+                    if ($.inArray(key, data) === -1)
+                    {
+                        data.push(key);
+                        $this.data(data_key, data.join(data_sep));
+                        current.handleObj.handler.apply( this, arguments );
+                    }
+                });
+            }
+        }
+
+        return _;
+    };
+    
 })(jQuery, jQuery.fn.domManip, jQuery.fn.html);
